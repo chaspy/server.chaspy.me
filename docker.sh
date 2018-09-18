e5e5afcb38f937ea0ba50514433d49bb55788c67
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-18 15:36:38 +0900

    Add docker-compose

--- docker.sh
+++ docker.sh
@@ -18,3 +18,8 @@ sudo apt-get install docker-ce -y
 
 docker -v
 sudo docker run hello-world
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
