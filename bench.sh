9580c7f5f856baf88fcc58d9850695a8c0ae4d41
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-25 11:29:26 +0900

    Add scripts for benchmark

--- bench.sh
+++ bench.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mkdir -p http/1.1
 for prot in h2c http/1.1; do
-  h2load https://server.chaspy.me -n 1000 -c 10 -p "$prot" > $prot.log
+  for num in 1000 10000 100000 1000000; do
+    h2load https://server.chaspy.me -n $num -c 10 -p "$prot" > $prot-$num.log
+    sleep 10
+  done
 done
