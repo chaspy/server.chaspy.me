011c356ff2b3a60dcaae5b8cc8d485596967828f
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-29 11:22:12 +0900

    Separate init script

--- script/init.sh
+++ script/init.sh
@@ -30,12 +30,3 @@ ufw allow 22/tcp
 ufw allow 80/tcp
 ufw allow 443/tcp
 
-# let's ecnrypt
-# Note: need name resolve
-# A Record server.chaspy.me to <IP addr>
-wget https://dl.eff.org/certbot-auto
-chmod a+x certbot-auto
-# https://community.letsencrypt.org/t/unable-to-lock-the-administration-directory-var-lib-dpkg/59168/4
-lsof /var/lib/dpkg/lock
-./certbot-auto certonly --standalone -d server.chaspy.me
-
